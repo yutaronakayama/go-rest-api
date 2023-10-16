eef6010350de71b9c7187c4db3e85dc31d8fd755
yutaro119 <nakayama.yutarotu2@gmail.com>
2023-10-16 09:12:44 +0900

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-rest-api
 
 go 1.21.0
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ internal/transport/http/handler.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	Router *mux.Router
+}
+
+func NewHanlder() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) SetupRputes() {
+	fmt.Println("Setting Up Routes")
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "I am alive")
+	})
+}

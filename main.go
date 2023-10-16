e75250cd541427f296261efa0c69917723f3d7fe
yutaro119 <nakayama.yutarotu2@gmail.com>
2023-10-16 09:17:30 +0900

    update

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/yutaronakayama/go-rest-api/internal/transport/http"
+)
 
 type App struct{}
 
-func (app *App) Run() error{
+func (app *App) Run() error {
 	fmt.Println("Setting Up Our App")
+
+	handler := transportHTTP.NewHanlder()
+	handler.SetupRputes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set yo server")
+		return err
+	}
+
 	return nil
 }
 
@@ -13,7 +27,7 @@ func main() {
 	fmt.Println("Go REST API Course")
 
 	app := App{}
-	if err := app.Run(); err !=nil{
+	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up our REST API")
 		fmt.Println(err)
 	}
